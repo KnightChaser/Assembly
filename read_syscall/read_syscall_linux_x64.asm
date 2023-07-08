3757559ac26b5c401e38e10ab9c63a16020b52a9
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-07-08 18:16:19 +0900

    Update read_syscall_linux_x64.asm
    fix inappropriate registers for x64

--- read_syscall/read_syscall_linux_x64.asm
+++ read_syscall/read_syscall_linux_x64.asm
@@ -9,20 +9,20 @@ section .text
 
 _start:
         ; read
-        mov eax, 0x0
+        mov rax, 0x0
         mov rdi, 0x0
         mov rsi, number
         mov rdx, numberLength
         syscall
 
         ; write
-        mov eax, 0x1
+        mov rax, 0x1
         mov rdi, 0x1
         mov rsi, number
         mov rdx, numberLength
         syscall
 
         ; exit
-        mov eax, 0x3C
+        mov rax, 0x3C
         mov rdi, 0x0
         syscall
