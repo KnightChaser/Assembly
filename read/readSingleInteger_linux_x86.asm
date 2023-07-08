859bdbd26d3abf214deb84238b5062daccb83cca
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-07-08 14:20:32 +0900

    Update readSingleInteger_linux_x86.asm

--- read/readSingleInteger_linux_x86.asm
+++ read/readSingleInteger_linux_x86.asm
@@ -1,25 +1,26 @@
 ; Nasm v2.15.05 (on Linux x86)
 
-section .data
-    integer times 4 db 0           ; 32-bits integer = 4 bytes
-    integerLength equ $-integer    ; the length of the variable integer
+section .bss
+    number RESB 0x4             ; int number;   (uninitialized)
+                                ; RESB(1 byte reserve) * 4 times = 4 bytes (int)
+    numberLength equ $-number
     
 section .text
     global _start
     
 _start:
     ; read(FILE_DESCRIPTOR, BUFFER, LENGTH)
-    mov eax, 0x3              ; read(
-    mov ebx, 0x0              ;     fd = stdin,
-    mov ecx, integer          ;     buf = integer,
-    mov edx, integerLength    ;     length = integerLength);
+    mov eax, 0x3             
+    mov ebx, 0x0              
+    mov ecx, number          
+    mov edx, numberLength  
     int 0x80
     
     ; write(FILE_DESCRIPTOR, BUFFER, LENGTH)
-    mov eax, 0x4              ; write(
-    mov ebx, 0x1              ;     fd = stdout,
-    mov ecx, integer          ;     buf = integer,
-    mov edx, integerLength    ;     length = integerLength);
+    mov eax, 0x4             
+    mov ebx, 0x1            
+    mov ecx, number        
+    mov edx, numberLength   
     int 0x80
     
     ; sys_exit()
