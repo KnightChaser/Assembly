b2a64f7eeebede9935c3d8069a7cbf5824493032
Lee Garam <47494005+KnightChaser@users.noreply.github.com>
2023-07-12 02:24:45 +0900

    Create conditions_a.c

---
+++ conditions/conditions_a.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+        int score = 75;
+
+        if (score >= 70) {
+                printf("PASS!");
+        } else {
+                printf("FAIL!");
+        }
+
+        return 0;
+
+}
